b47b63b3e138d4554b9ce076e05334b4a090f6e0
GoodEvening07 <gabrielmelendez33@gmail.com>
2023-07-30 21:13:01 -0400

    This is my implementation for lab 9.

--- lab9.c
+++ lab9.c
@@ -1,4 +1,15 @@
+/*
+	Gabriel Hernandez
+	COP3502C - CS1 Lab 9
+	7-30-2023
+*/
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+
 
 // RecordType
 struct RecordType
@@ -11,13 +22,15 @@ struct RecordType
 // Fill out this structure
 struct HashType
 {
-
+	struct RecordType record;
+	struct HshType* next;
 };
+
 
 // Compute the hash function
 int hash(int x)
 {
-
+	return x % 15;
 }
 
 // parses input file to an integer array
@@ -64,9 +77,22 @@ void printRecords(struct RecordType pData[], int dataSz)
 	printf("\nRecords:\n");
 	for (i = 0; i < dataSz; ++i)
 	{
-		printf("\t%d %c %d\n", pData[i].id, pData[i].name, pData[i].order);
+		printf("\tIndex:%d Name:%c Order%d\n", pData[i].id, pData[i].name, pData[i].order);
 	}
 	printf("\n\n");
+}
+
+//New function//
+void SettingHash(struct HashType* t)
+{
+	for (int i = 0; i < 15; i++)
+	{
+		t[i].record.id = 0;
+		t[i].record.order = 0;
+		t[i].record.name = '-';
+		t[i].next = NULL;
+
+	}
 }
 
 // display records in the hash structure
@@ -75,20 +101,58 @@ void printRecords(struct RecordType pData[], int dataSz)
 // index x -> id, name, order -> id, name, order ....
 void displayRecordsInHash(struct HashType *pHashArray, int hashSz)
 {
-	int i;
+		int i;
 
-	for (i=0;i<hashSz;++i)
-	{
-		// if index is occupied with any records, print all
-	}
+	   for (i = 0; i < hashSz; i++)
+    {
+        if (pHashArray[i].record.id != 0)
+        {
+            printf("index %d -> id:%d, name:%c, order:%d", i, pHashArray[i].record.id, pHashArray[i].record.name, pHashArray[i].record.order);
+            struct HashType* current = pHashArray[i].next;
+            while (current != NULL)
+            {
+                printf(" -> id:%d, name:%c, order:%d", current->record.id, current->record.name, current->record.order);
+                current = current->next;
+            }
+            printf("\n");
+        }
+    }
 }
 
-int main(void)
+int main()
 {
 	struct RecordType *pRecords;
 	int recordSz = 0;
 
 	recordSz = parseData("input.txt", &pRecords);
 	printRecords(pRecords, recordSz);
-	// Your hash implementation
+
+	// Implement the hash table
+	struct HashType* hashTable = (struct HashType*)malloc(sizeof(struct HashType) * 15);
+    SettingHash(hashTable);
+
+    // Fill the hash table with the records from pRecords
+    for (int i = 0; i < recordSz; ++i)
+    {
+        int index = hash(pRecords[i].order);
+        if (hashTable[index].record.id == 0)
+        {
+            // No collision
+            hashTable[index].record = pRecords[i];
+        }
+        else
+        {
+            // Collision: add the new record to the linked list at the index
+            struct HashType* newNode = (struct HashType*)malloc(sizeof(struct HashType));
+            newNode->record = pRecords[i];
+            newNode->next = hashTable[index].next;
+            hashTable[index].next = newNode;
+        }
+    }
+
+    displayRecordsInHash(hashTable, 15);
+
+    // Free memory
+    free(hashTable);
+    free(pRecords);
 }
